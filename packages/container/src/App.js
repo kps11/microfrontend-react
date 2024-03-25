310df32d2442a1fe65aa509dd1c4196077bbd1f5
kps11 <kpsenapati6@gmail.com>
2024-03-25 13:32:15 +0530

    adding config to project

---
+++ packages/container/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import MarketingApp from "./components/MarketingApp";
+
+function App() {
+  return (
+    <div>
+      <h1>Hi there !!! lknvdlksnvdkl</h1>
+      <hr />
+      <MarketingApp />
+    </div>
+  );
+}
+
+export default App;
+
+// node-modules
+// dist
+// .DS_Store
