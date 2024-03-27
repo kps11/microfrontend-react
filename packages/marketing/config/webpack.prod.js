adaa2702825695517871a25832cca4c30c96d591
kps11 <kpsenapati6@gmail.com>
2024-03-27 07:01:58 +0530

    config settings for marketing app

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -7,6 +7,7 @@ const prodConfig = {
   name: "production",
   output: {
     filename: "[name].[contenthash].js",
+    publicPath: "/marketing/latest/",
   },
   plugins: [
     new ModuleFederationPlugin({
