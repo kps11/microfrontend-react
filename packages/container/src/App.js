0a8a1f7274f9042d772daecbcd866adf5d452cf2
kps11 <kpsenapati6@gmail.com>
2024-03-28 05:29:15 +0530

    adding header to app

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div>
-      <h1>Welcome to our App !!</h1>
-      <hr />
-      <MarketingApp />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <MarketingApp />
+      </div>
+    </BrowserRouter>
   );
 }
 
